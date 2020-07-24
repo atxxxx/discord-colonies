5065fea0096ffd8227a942d4628d78fa54dfc92c
Bears9355 <52669057+Bears9355@users.noreply.github.com>
2020-07-24 17:38:54 +0200

    Gros bug x)

--- files/msgMine.js
+++ files/msgMine.js
@@ -1,164 +1,188 @@
+const moment = require("moment");
+
 module.exports = (message, msgSplit, client, config, Discord, connection) => {
 
-    if (msgSplit[0].toLowerCase() != config.prefix + "craft") return;
+    if (msgSplit[0].toLowerCase() != config.prefix + "mine") return;
 
-    if (!msgSplit[1]) {
-        let embed = new Discord.MessageEmbed()
-            .setColor("#57b8e2")
-            .setAuthor("Craft de " + message.author.tag, message.author.avatarURL({ format: "png" }))
-            .setDescription("Liste des items à craft : \n`table`, `pioche`")
+    if (message.channel.id !== "701720240207167520") {
 
-        message.channel.send("Merci de préciser un objet à crafter")
-        message.channel.send(embed)
+        message.channel.send("Il faut faire la commande dans le channel mine !");
         return;
     }
 
-    if (msgSplit[1] == "table") {
+    if (!client.cooldowns.has("mine")) client.cooldowns.set("mine", new Discord.Collection());
+    if (!client.cooldowns.get("mine").has(message.author.id)) {
 
-        connection.query("SELECT * FROM inventory WHERE Member_ID=?", [message.author.id], (error, result) => {
+        client.cooldowns.get("mine").set(message.author.id, message.createdTimestamp);
 
-            if (error) {
+    } else {
 
-                console.log("Erreur MySQL - cmdCraft.js - inventory - \"SELECT * FROM inventory WHERE Member_ID=?\" !");
-                message.channel.send("Une erreur est survenue.");
+        if ((client.cooldowns.get("mine").get(message.author.id) + (5 * 60 * 1000)) > message.createdTimestamp) {
+
+            if (!message.member.hasPermission("ADMINISTRATOR")) {
+
+                let mins = moment((client.cooldowns.get("mine").get(message.author.id) + (5 * 60 * 1000)) - message.createdTimestamp).format("mm");
+                let secs = moment((client.cooldowns.get("mine").get(message.author.id) + (5 * 60 * 1000)) - message.createdTimestamp).format("ss");
+
+                message.channel.send(":x: **Il te faut attendre encore " + mins + " minute et " + secs + " secondes pour pouvoir reminer !**");
                 return;
             }
 
-            let wooduser = result[0].Wood
+        } else {
 
-            if (result[0].CraftingTable == "Non") {
-
-                if (result[0].Wood >= "2") {
-
-                    connection.query("UPDATE inventory SET Wood=? WHERE Member_ID=?", [wooduser - 2, message.author.id], (error, result) => {
-
-                        if (error) {
-
-                            console.log("Erreur MySQL - cmdCraft - \"SELECT * FROM inventory WHERE Member_ID=?\" !");
-                            message.channel.send("**:x: Une erreur est survenue, réessayez plus tard !**");
-                            return;
-                        }
-
-                        message.channel.send("*Crafting en cours...*")
-
-                        connection.query("UPDATE inventory SET CraftingTable=? WHERE Member_ID=?", ["Oui", message.author.id], (error, result) => {
-
-                            if (error) {
-
-                                console.log("Erreur MySQL - cmdCraft - \"UPDATE inventory SET CraftingTable=? WHERE Member_ID=?\" !");
-                                message.channel.send("**:x: Une erreur est survenue, réessayez plus tard !**");
-                                return;
-                            }
-
-                            message.channel.send("**Table de craft craftée !**")
-                        })
-                    })
-
-                } else {
-
-                    message.channel.send("Vous n'avez pas assez de bois !")
-                    return;
-                }
-
-            } else {
-
-                message.channel.send("Vous avez déjà une table de craft !")
-                return;
-            }
-        })
+            client.cooldowns.get("mine").set(message.author.id, message.createdTimestamp);
+        }
     }
 
-    // Craft des pioches
-    if (msgSplit[1] == "pioche") {
+    connection.query("SELECT * FROM inventory WHERE Member_ID=?", [message.author.id], (error, result) => {
 
-        connection.query("SELECT * FROM inventory WHERE Member_ID=?", [message.author.id], (error, result) => {
+        if (error) {
 
-            if (error) {
+            console.log("Erreur MySQL - msgMine - \"SELECT * FROM inventory WHERE Member_ID=?\" !");
+            return;
+        }
 
-                console.log("Erreur MySQL - cmdCraft - \"SELECT * FROM inventory WHERE Member_ID=?, ligne 83\" !");
-                message.channel.send("**:x: Une erreur est survenue, réessayez plus tard !**");
-                return;
-            }
+        if (result[0].Pickaxe == "Bois") {
 
-            // Si il a une table de craft
-            if (result[0].CraftingTable == "Non") {
+            // coal
+            let mincoal = 1;
+            let maxcoal = 10;
+            let coalToAdd = Math.random() * (maxcoal - mincoal) + mincoal;
+            let coaluser = result[0].Coal
+            // diamond
+            let mindiamond = 0;
+            let maxdiamond = 0;
+            let diamondToAdd = Math.random() * (maxdiamond - mindiamond) + mindiamond;
+            let diamonduser = result[0].Diamond
+            // gold
+            let mingold = 0;
+            let maxgold = 0;
+            let goldToAdd = Math.random() * (maxgold - mingold) + mingold;
+            let golduser = result[0].Gold
+            // iron
+            let miniron = 0;
+            let maxiron = 6;
+            let ironToAdd = Math.random() * (maxiron - miniron) + miniron;
+            let ironuser = result[0].Iron
+            // pierre 
+            let minpierre = 0;
+            let maxpierre = 5;
+            let pierreToAdd = Math.random() * (maxpierre - minpierre) + minpierre;
+            let pierreuser = result[0].Rock
 
-                message.channel.send("Tu dois avoir une table de craft !");
-                return;
-            }
+            connection.query("UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Rock=? WHERE Member_ID=?", [diamonduser + diamondToAdd, golduser + goldToAdd, ironuser + ironToAdd, coaluser + coalToAdd, pierreuser + pierreToAdd, message.author.id], (error, result) => {
 
-            // Si il a une pioche en bois
-            if (result[0].Pickaxe == "Bois") {
-                message.channel.send("La pioche coûte 15 de fer !")
+                if (error) {
 
-                // Si il a assez de fer
-                if (result[0].Iron >= "15") {
-
-                    let ironuser = result[0].Iron
-
-                    connection.query("UPDATE inventory SET Iron=? WHERE Member_ID=?", [ironuser - 15, message.author.id], (error, result) => {
-
-                        if (error) {
-
-                            console.log("Erreur MySQL - cmdCraft - \"UPDATE inventory SET Pickaxe=? WHERE Member_ID=?, ligne 106\" !");
-                            message.channel.send("**:x: Une erreur est survenue, réessayez plus tard !**");
-                            return;
-                        }
-
-                        message.channel.send("*Crafting en cours...*")
-
-                        connection.query("UPDATE inventory SET Pickaxe=? WHERE Member_ID=?", ["Fer", message.author.id], (error, result) => {
-
-                            if (error) {
-
-                                console.log("Erreur MySQL - cmdCraft - \"UPDATE inventory SET CraftingTable=? WHERE Member_ID=?\" !");
-                                message.channel.send("**:x: Une erreur est survenue, réessayez plus tard !**");
-                                return;
-                            }
-
-                            message.channel.send("**Pioche en fer craftée !**")
-
-
-                        })
-                    })
-                } else {
-                    message.channel.send("Tu n'a pas assez de fer !");
-                    return;
+                    console.log("Erreur MySQL - msgMine.js - inventory - \"UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=? WHERE Member_ID=?\" !");
                 }
-            }
 
-            if(result[0].Pickaxe == "Fer") {
-                message.channel.send("La pioche coûte 25 d'or !");
-                if(result[0].Gold >= "25") {
-                    let golduser = result[0].Gold
-                    connection.query("UPDATE inventory SET Gold=? WHERE Member_ID=?", [golduser - 25, message.author.id], (error, result) => {
+                message.react("✅");
+            });
+        }
 
-                        if (error) {
+        if (result[0].Pickaxe == "Fer") {
 
-                            console.log("Erreur MySQL - cmdCraft - \"UPDATE inventory SET Gold=? WHERE Member_ID=?, ligne 132\" !");
-                            message.channel.send("**:x: Une erreur est survenue, réessayez plus tard !**");
-                            return;
-                        }
+            // coal
+            let mincoal = 3;
+            let maxcoal = 15;
+            let coalToAdd = Math.random() * (maxcoal - mincoal) + mincoal;
+            let coaluser = result[0].Coal
+            // diamond
+            let mindiamond = 0;
+            let maxdiamond = 2;
+            let diamondToAdd = Math.random() * (maxdiamond - mindiamond) + mindiamond;
+            let diamonduser = result[0].Diamond
+            // gold
+            let mingold = 0;
+            let maxgold = 4;
+            let goldToAdd = Math.random() * (maxgold - mingold) + mingold;
+            let golduser = result[0].Gold
+            // iron
+            let miniron = 0;
+            let maxiron = 6;
+            let ironToAdd = Math.random() * (maxiron - miniron) + miniron;
+            let ironuser = result[0].Iron
+            // atitix
+            let minatitix = 0;
+            let maxatitix = 1;
+            let atitixToAdd = Math.random() * (maxatitix - minatitix) + minatitix
+            let atitixuser = result[0].Atitix
+            // pierre 
+            let minpierre = 0;
+            let maxpierre = 9;
+            let pierreToAdd = Math.random() * (maxpierre - minpierre) + minpierre;
+            let pierreuser = result[0].Rock
+            // Mythril
+            let minmythril = 0;
+            let maxmythril = 2;
+            let mythrilToAdd = Math.random() * (maxmythril - minmythril) + minmythril;
+            let mythriluser = result[0].Mythril
 
-                        message.channel.send("*Crafting en cours...*")
 
-                        connection.query("UPDATE inventory SET Pickaxe=? WHERE Member_ID=?", ["Or", message.author.id], (error, result) => {
+            connection.query("UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=?, Mythril=? WHERE Member_ID=?", [diamonduser + diamondToAdd, golduser + goldToAdd, ironuser + ironToAdd, coaluser + coalToAdd, atitixuser + atitixToAdd, pierreuser + pierreToAdd, mythriluser + mythrilToAdd, message.author.id], (error, result) => {
 
-                            if (error) {
+                if (error) {
 
-                                console.log("Erreur MySQL - cmdCraft - \"UPDATE inventory SET Pickaxe=? WHERE Member_ID=?, ligne 143\" !");
-                                message.channel.send("**:x: Une erreur est survenue, réessayez plus tard !**");
-                                return;
-                            }
-
-                            message.channel.send("**Pioche en or craftée !**")
-
-
-                        })
-
-                    })
+                    console.log("Erreur MySQL - msgMine.js - inventory - IronPickaxe \"UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=?, Mytrhil=? WHERE Member_ID=?\" !");
                 }
-            }
-        })
-    }
+
+                message.react("✅");
+            });
+        }
+        if (result[0].Pickaxe == "Or") {
+            // coal
+            let mincoal = 5;
+            let maxcoal = 20;
+            let coalToAdd = Math.random() * (maxcoal - mincoal) + mincoal;
+            let coaluser = result[0].Coal
+            // diamond
+            let mindiamond = 0;
+            let maxdiamond = 5;
+            let diamondToAdd = Math.random() * (maxdiamond - mindiamond) + mindiamond;
+            let diamonduser = result[0].Diamond
+            // gold
+            let mingold = 0;
+            let maxgold = 7;
+            let goldToAdd = Math.random() * (maxgold - mingold) + mingold;
+            let golduser = result[0].Gold
+            // iron
+            let miniron = 0;
+            let maxiron = 10;
+            let ironToAdd = Math.random() * (maxiron - miniron) + miniron;
+            let ironuser = result[0].Iron
+            // atitix
+            let minatitix = 0;
+            let maxatitix = 1;
+            let atitixToAdd = Math.random() * (maxatitix - minatitix) + minatitix
+            let atitixuser = result[0].Atitix
+            // pierre 
+            let minpierre = 0;
+            let maxpierre = 12;
+            let pierreToAdd = Math.random() * (maxpierre - minpierre) + minpierre;
+            let pierreuser = result[0].Rock
+            // Mythril
+            let minmythril = 0;
+            let maxmythril = 3;
+            let mythrilToAdd = Math.random() * (maxmythril - minmythril) + minmythril;
+            let mythriluser = result[0].Mythril
+            // Amethyst
+            let minamethyst = 0;
+            let maxamethyst = 1;
+            let amethystToAdd = Math.random() * (maxamethyst - minamethyst) + minamethyst
+            let amethystuser = result[0].Amethyst
+
+
+            connection.query("UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=?, Mythril=?, Amethyst=? WHERE Member_ID=?", [diamonduser + diamondToAdd, golduser + goldToAdd, ironuser + ironToAdd, coaluser + coalToAdd, atitixuser + atitixToAdd, pierreuser + pierreToAdd, mythriluser + mythrilToAdd, amethystuser + amethystToAdd, message.author.id], (error, result) => {
+
+                if (error) {
+
+                    console.log("Erreur MySQL - msgMine.js - inventory - Goldepickaxe \"UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=?, Mythril=?, Amethyst=? WHERE Member_ID=?\" !");
+                }
+
+                message.react("✅");
+
+            })
+        }
+    });
 }
