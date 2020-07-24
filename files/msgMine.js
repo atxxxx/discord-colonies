1b3d6d31871d0f35e9e826a6039ac57fc165c06d
Bears9355 <52669057+Bears9355@users.noreply.github.com>
2020-07-24 17:28:17 +0200

    Ajout de l'améthyste

--- files/msgMine.js
+++ files/msgMine.js
@@ -130,5 +130,59 @@ module.exports = (message, msgSplit, client, config, Discord, connection) => {
                 message.react("✅");
             });
         }
+        if (result[0].Pickaxe == "Or") {
+            // coal
+            let mincoal = 5;
+            let maxcoal = 20;
+            let coalToAdd = Math.random() * (maxcoal - mincoal) + mincoal;
+            let coaluser = result[0].Coal
+            // diamond
+            let mindiamond = 0;
+            let maxdiamond = 5;
+            let diamondToAdd = Math.random() * (maxdiamond - mindiamond) + mindiamond;
+            let diamonduser = result[0].Diamond
+            // gold
+            let mingold = 0;
+            let maxgold = 7;
+            let goldToAdd = Math.random() * (maxgold - mingold) + mingold;
+            let golduser = result[0].Gold
+            // iron
+            let miniron = 0;
+            let maxiron = 10;
+            let ironToAdd = Math.random() * (maxiron - miniron) + miniron;
+            let ironuser = result[0].Iron
+            // atitix
+            let minatitix = 0;
+            let maxatitix = 1;
+            let atitixToAdd = Math.random() * (maxatitix - minatitix) + minatitix
+            let atitixuser = result[0].Atitix
+            // pierre 
+            let minpierre = 0;
+            let maxpierre = 12;
+            let pierreToAdd = Math.random() * (maxpierre - minpierre) + minpierre;
+            let pierreuser = result[0].Rock
+            // Mythril
+            let minmythril = 0;
+            let maxmythril = 3;
+            let mythrilToAdd = Math.random() * (maxmythril - minmythril) + minmythril;
+            let mythriluser = result[0].Mythril
+            // Amethyst
+            let minamethyst = 0;
+            let maxamethyst = 1;
+            let amethystToAdd = Math.random() * (maxamethyst - minamethyst) + minamethyst
+            let amethystuser = result[0].Amethyst
+
+
+            connection.query("UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=?, Mythril=?, Amethyst=? WHERE Member_ID=?", [diamonduser + diamondToAdd, golduser + goldToAdd, ironuser + ironToAdd, coaluser + coalToAdd, atitixuser + atitixToAdd, pierreuser + pierreToAdd, mythriluser + mythrilToAdd, amethystuser + amethystToAdd, message.author.id], (error, result) => {
+
+                if (error) {
+
+                    console.log("Erreur MySQL - msgMine.js - inventory - Goldepickaxe \"UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=?, Mythril=?, Amethyst=? WHERE Member_ID=?\" !");
+                }
+
+                message.react("✅");
+
+            })
+        }
     });
 }
