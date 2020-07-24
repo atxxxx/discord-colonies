02ec04f130644390e3f68e5ea7415538d5b3e26f
Bears9355 <52669057+Bears9355@users.noreply.github.com>
2020-07-24 15:41:10 +0200

    Ajout du Mytrhil

--- files/msgMine.js
+++ files/msgMine.js
@@ -113,12 +113,18 @@ module.exports = (message, msgSplit, client, config, Discord, connection) => {
             let maxpierre = 9;
             let pierreToAdd = Math.random() * (maxpierre - minpierre) + minpierre;
             let pierreuser = result[0].Rock
+            // Mythril
+            let minmythril = 0;
+            let maxmythril = 2;
+            let mythrilToAdd = Math.random() * (maxmythril - minmythril) + minmythril;
+            let mythriluser = result[0].Mythril
 
-            connection.query("UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=? WHERE Member_ID=?", [diamonduser + diamondToAdd, golduser + goldToAdd, ironuser + ironToAdd, coaluser + coalToAdd, atitixuser + atitixToAdd, pierreuser + pierreToAdd, message.author.id], (error, result) => {
+
+            connection.query("UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=?, Mytrhil=? WHERE Member_ID=?", [diamonduser + diamondToAdd, golduser + goldToAdd, ironuser + ironToAdd, coaluser + coalToAdd, atitixuser + atitixToAdd, pierreuser + pierreToAdd, mythriluser + mythrilToAdd, message.author.id], (error, result) => {
 
                 if (error) {
 
-                    console.log("Erreur MySQL - msgMine.js - inventory - IronPickaxe \"UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=? WHERE Member_ID=?\" !");
+                    console.log("Erreur MySQL - msgMine.js - inventory - IronPickaxe \"UPDATE inventory SET Diamond=?, Gold=?, Iron=?, Coal=?, Atitix=?, Rock=?, Mytrhil=? WHERE Member_ID=?\" !");
                 }
 
                 message.react("✅");
